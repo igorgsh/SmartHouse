2738bb0f818e9f19fbdf518e0b094aec1cea56b9
Igor Shevchenko <igorgsh@gmail.com>
2017-01-27 22:13:53 +0200

    Switch to MQTT

---
+++ SmartServer/webservice0/src/main/java/sigma/smarthouse/server/webservice/Application.java
@@ -0,0 +1,11 @@
+package sigma.smarthouse.server.webservice;
+
+/**
+ * Created by Igor Shevchenko on 22.01.17.
+ */
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+
+public class Application {
+}
