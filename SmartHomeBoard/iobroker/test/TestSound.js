51b723a40772e4bb17f43cd736775414501b6b49
root <igorgsh@gmail.com>
2021-11-25 16:56:13 +0200

    changes

--- SmartHomeBoard/iobroker/test/TestSound.js
+++ SmartHomeBoard/iobroker/test/TestSound.js
@@ -2,5 +2,5 @@ setState('vis.0.control.instance', 'FFFFFFFF');
 setState('vis.0.control.data', 'http://www.modular-planet.de/fx/marsians/Marsiansrev.mp3');
 //setState('vis.0.control.data', 'message;title;alert');
 setState('vis.0.control.command', 'playSound');
-//setState('vis.0.control.command', 'alert');   
+//setState('vis.0.control.command', 'alert');
  
