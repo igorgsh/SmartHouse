5ef9433da0d63ed7e6bcb4b26dcd740aacc5ca86
Igor Shevchenko <igorgsh@gmail.com>
2021-11-28 09:51:36 +0200

    Revert "Script Changes"
    This reverts commit 02b1ccdf5b8f63fd1dcefcb4b54e7b482b9c9187.

---
+++ SmartHomeBoard/iobroker/common/PowerCollecting.js
@@ -0,0 +1,90 @@
+var ElectricityGroup = [
+                        {"aggregation": "mqtt.0.Others.Heating", "sensor": 
+                                                                [prefixPowerMeter + ".P0264",
+                                                                 prefixPowerMeter + ".P0265",
+                                                                 prefixPowerMeter + ".P0266"]},
+                        {"aggregation": "mqtt.0.Others.Electricity", "sensor": 
+                                                                [prefixPowerMeter + ".P0264",
+                                                                 prefixPowerMeter + ".P0265",
+                                                                 prefixPowerMeter + ".P0266"]}
+];  
+
+
+ElectricityGroup.forEach(function(item, i, ElectricityGroup) {
+  var str = '';
+  for (var j=0; j<3 ; j++) {
+    str = item.sensor[j] + ".Voltage";
+    $(str).on(function(obj){
+        ProcessEnergy(obj.id);
+    });
+    str = item.sensor[j] + ".Power";
+    $(str).on(function(obj){
+        ProcessEnergy(obj.id);
+    });
+    str = item.sensor[j] + ".Energy";
+    $(str).on(function(obj){
+        ProcessEnergy(obj.id);
+    });
+  }
+});
+function ProcessEnergy(nm) {
+    var aggr = ElectricityGroup.filter(function(el){
+        for (var i=0; i<3 ; i++) {
+            if (nm.startsWith(el.sensor[i])) {
+                return true; 
+            }
+        }
+        return false;
+    });
+  
+    aggr.forEach(function(item, i, aggr) {
+        var sens = item.sensor.filter(function(el){
+            return (nm.startsWith(el));
+        });
+
+        var suff = nm.substring(sens[0].length + 1);
+        var v1, v2, v3;
+        switch (suff) {
+            case 'Voltage':
+                var v1, v2, v3;
+                v1 = Number(getState(item.sensor[0]+".Voltage").val);
+                v2 = Number(getState(item.sensor[1]+".Voltage").val);
+                v3 = Number(getState(item.sensor[2]+".Voltage").val);
+
+                var mn = Math.min(v1, v2, v3);
+                var mx = Math.max(v1, v2, v3);
+                setState(item.aggregation+".Max", Number(mx));
+                setState(item.aggregation+".Min", Number(mn));
+                setState(item.aggregation+".Delta", Number(mx-mn));
+                setState(item.aggregation+".LineA", Number(v1));
+                setState(item.aggregation+".LineB", Number(v2));
+                setState(item.aggregation+".LineC", Number(v3));
+                break;
+            case 'Power':
+            case 'Energy':
+                var p = Number(getState(item.sensor[0] + '.' + suff).val) + 
+                    Number(getState(item.sensor[1] + '.' + suff).val) + 
+                    Number(getState(item.sensor[2] + '.' + suff).val); 
+                setState(item.aggregation + '.' + suff, p);
+                var v1, v2, v3;
+                v1 = Number(getState(item.sensor[0]+".Power").val);
+                v2 = Number(getState(item.sensor[1]+".Power").val);
+                v3 = Number(getState(item.sensor[2]+".Power").val);
+                setState(item.aggregation+".PowerA", Number(v1));
+                setState(item.aggregation+".PowerB", Number(v2));
+                setState(item.aggregation+".PowerC", Number(v3));
+                v1 = Number(getState(item.sensor[0]+".Energy").val);
+                v2 = Number(getState(item.sensor[1]+".Energy").val);
+                v3 = Number(getState(item.sensor[2]+".Energy").val);
+                setState(item.aggregation+".EnergyA", Number(v1));
+                setState(item.aggregation+".EnergyB", Number(v2));
+                setState(item.aggregation+".EnergyC", Number(v3));
+                
+                break;
+        } 
+    });
+} 
+
+
+
+
